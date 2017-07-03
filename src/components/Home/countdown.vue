bdf81f454232e0f7311a0fda72ae7faa7c71f91c
Anudle <AnuShrestha007@gmail.com>
2017-07-02 20:50:32 -0600

    fixed filter bug

--- src/components/Home/countdown.vue
+++ src/components/Home/countdown.vue
@@ -1,26 +1,44 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Marketplace
+Gist
+ @Anudle
+ Sign out
+ Unwatch 2
+  Star 0
+ Fork 1 oconnerm/wedding
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki Insights
+Tree: d8e40f4240 Find file Copy pathwedding/src/components/Home/countdown.vue
+23cd7f2  3 days ago
+@Anudle Anudle added countdown and extra party section
+1 contributor
+RawBlameHistory
+88 lines (82 sloc)  1.77 KB
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-sm-3">
+      <div class="col-md-3">
         <div class="block">
           <p class="digit">{{ days | two_digits }}</p>
           <p class="text">Days</p>
         </div>
       </div>
-      <div class="col-sm-3">
+      <div class="col-md-3">
         <div class="block">
           <p class="digit">{{ hours | two_digits }}</p>
           <p class="text">Hours</p>
         </div>
       </div>
-      <div class="col-sm-3">
+      <div class="col-md-3">
         <div class="block">
           <p class="digit">{{ minutes | two_digits }}</p>
           <p class="text">Minutes</p>
         </div>
       </div>
-      <div class="col-sm-3">
-        <div class="block">
+      <div class="col-md-3">
           <p class="digit">{{ seconds | two_digits }}</p>
           <p class="text">Seconds</p>
         </div>
@@ -30,18 +48,25 @@
 </template>
 
 <script>
+import Vue from 'vue'
+
+Vue.filter('two_digits', function (value) {
+  if (value.toString().length <= 1) {
+    return '0' + value.toString()
+  }
+  return value.toString()
+})
+
 export default {
   props: {
     date: null
   },
-
   data () {
     return {
       now: Math.trunc((new Date()).getTime() / 1000),
       event: this.date
     }
   },
-
   computed: {
     calculatedDate () {
       this.event = Math.trunc(Date.parse(this.event) / 1000)
@@ -51,7 +76,6 @@ export default {
       return (this.calculatedDate - this.now) % 60
     },
     minutes () {
-      console.log(Math.trunc((this.calculatedDate - this.now)))
       return Math.trunc((this.calculatedDate - this.now) / 60) % 60
     },
     hours () {
@@ -61,7 +85,6 @@ export default {
       return Math.trunc((this.calculatedDate - this.now) / 60 / 60 / 24)
     }
   },
-
   mounted () {
     window.setInterval(() => {
       this.now = Math.trunc((new Date()).getTime() / 1000)
@@ -86,3 +109,5 @@ export default {
   text-align: center;
 }
 </style>
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
--- src/components/RSVP/RsvpSection.vue
+++ src/components/RSVP/RsvpSection.vue
@@ -1,47 +1,16 @@
 <template lang="html">
-  <div id="app" class="container">
-    <h1>Join Ryan and Becca!</h1>
-    <h2>Find your RSVP by typing in your Code:</h2>
-    <button @click.prevent="submit">Submit</button>
-    <input v-model="guestInput" placeholder="">
-  </div>
-
+  <submit></submit>
 </template>
 
 <script>
 
-import Firebase from 'Firebase'
-
-let config = {
-  apiKey: 'AIzaSyCctzH9u49O2WlgUjZR2sTC7lcdQ3xgnYQ',
-  authDomain: 'wedding-7f971.firebaseapp.com',
-  databaseURL: 'https://wedding-7f971.firebaseio.com',
-  projectId: 'wedding-7f971',
-  storageBucket: 'wedding-7f971.appspot.com',
-  messagingSenderId: '117898341533'
-}
-
-let app = Firebase.initializeApp(config)
-let db = app.database()
-let guestCode = db.ref('gues_code')
+import submit from './submit.vue'
 
 export default {
-  methods: {
-    submit: function () {
-      var codeQuery = Firebase.database().ref('guest_code').child(this.guestInput)
-      codeQuery.once('value').then(function (snapshot) {
-        alert(snapshot.val())
-      })
-    }
-  },
-  firebase: {
-    guest: guestCode
-  },
-  data () {
-    return {
-      guestInput: ''
-    }
+  components: {
+    submit
   }
+
 }
 
 </script>
