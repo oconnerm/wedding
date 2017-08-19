2b748d179610731de9cebe426ec02a7a295a4cc4
Anudle <AnuShrestha007@gmail.com>
2017-08-19 13:22:30 -0600

    cleaned up wedding section

---
+++ src/components/Wedding/locationInfo.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="">
+    <p class='header'>The wedding will be held on Friday, August 10, 2018. We'll start at the Evergreen Wedding Terrace for the ceremony, then move to the ballroom for the reception.</p>
+    <br>
+    <p>Ceremoney: 5:00pm - 6:00pm</p>
+    <p>Reception: 7:00pm - 12:00pm</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+  .header{
+    margin: auto;
+    margin-top: 5vh;
+    width: 60vw;
+  }
+  p{
+    margin: 0px;
+  }
+</style>
