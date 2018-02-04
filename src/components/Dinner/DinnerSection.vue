517e460201dc7fab5e4c510d05ff5aead2d4445b
Anudle <AnuShrestha007@gmail.com>
2018-02-03 18:46:43 -0700

    dinner updated

--- src/components/Dinner/DinnerSection.vue
+++ src/components/Dinner/DinnerSection.vue
@@ -1,11 +1,19 @@
 <template lang="html">
-
+  <div class="">
+    <h1>Dinner</h1>
+    <dinner></dinner>
+  </div>
 </template>
 
 <script>
+import dinner from './dinnerRSVP.vue'
 export default {
+  components: {
+    dinner
+  }
 }
 </script>
 
 <style lang="css">
+
 </style>
